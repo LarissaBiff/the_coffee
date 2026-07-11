ea8343d897a93e2d1e1905c56bb20411185c827e
Luiz Henrique Biff <biff@fesctecnologia.com.br>
2026-07-11 11:02:42 -0300

    ...

--- src/botoes/seeCoffees.vue
+++ src/botoes/seeCoffees.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <div></div>
+    <button href="#">
+      <font-awesome-icon icon="fa-solid fa-mug-hot" style="font-size: 20px;" />
+      Ver Cafés
+    </button>
 </template>
 
-<style></style>
+<style scoped>
+button{
+  background-color: rgb(104, 57, 38);
+  color: whitesmoke;
+  border-radius: 5px;
+  border: none;
+  padding: 10px 5vw;
+  font-size: 15px;
+  display: flex;
+  align-items: flex-end;
+  gap: 8px;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+}
+button:hover{
+  transform: scale(1.05);
+}
+button:active{
+  transform: scale(1.15);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import seeCoffees from '@/botoes/seeCoffees.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <seeCoffees/>
   </main>
 </template>
