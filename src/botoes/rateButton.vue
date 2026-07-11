f5e9133eedeed52863784cb35535bd74ad5df36b
Luiz Henrique Biff <biff@fesctecnologia.com.br>
2026-07-11 11:36:24 -0300

    ...

---
+++ src/botoes/rateButton.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+    <button href="#">
+        Avaliar
+    </button>
+</template>
+
+<style scoped>
+button{
+  background-color: rgba(0, 0, 128, 0);
+  border: 2.5px solid rgb(104, 57, 38);
+  color: rgb(104, 57, 38);
+  border-radius: 5px;
+  padding: 12px 2vw;
+  font-size: 15px;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+}
+button:hover{
+  transform: scale(1.05);
+}
+button:active{
+  transform: scale(1.15);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import seeCoffees from '@/botoes/seeCoffees.vue';
+import rateButton from '@/botoes/rateButton.vue';
 </script>
 
 <template>
   <main>
     <seeCoffees/>
+    <rateButton/>
   </main>
 </template>
