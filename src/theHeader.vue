6c4472abab7f8ea0b096d8851ab7d1f7d1e11f19
LarissaBiff <bifflarissa@gmail.com>
2026-07-12 20:28:57 -0300

    Starting the header - on html and css part (just finished the html); editing 'main.css' (because of the width);

--- src/theHeader.vue
+++ src/theHeader.vue
@@ -1,10 +1,39 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <main>
-  </main>
+  <header>
+    <nav>
+        <div class="menu">
+            <h1>
+            <RouterLink to="/">
+                <font-awesome-icon icon="fa-solid fa-mug-hot" style="font-size: 20px; color: wheat;"/> 
+                The C.Q.C.
+            </RouterLink>
+        </h1>
+
+        <div class="pages">
+            <RouterLink to="/">HOME</RouterLink>
+            <RouterLink to="/cafes">COFFEES</RouterLink>
+            <RouterLink to="/avaliar">RATE</RouterLink>
+            <RouterLink to="/ranking">RANKING</RouterLink>
+        </div>
+        </div>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
+header {
+  width: 100%;
+  left: 0;
+  top: 0;
+  background-color: rgb(104, 57, 38);
+}
+.menu{
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+}
 </style>
