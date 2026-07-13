9158450666f0c6725851b607e7ba4bd4593c24dd
LarissaBiff <bifflarissa@gmail.com>
2026-07-13 19:56:11 -0300

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,7 @@
 <script setup>
-import seeCoffees from '@/botoes/seeCoffees.vue';
-import rateButton from '@/botoes/rateButton.vue';
-import saveButton from '@/botoes/saveButton.vue';
 </script>
 
 <template>
   <main>
-    <seeCoffees/>
-    <rateButton/>
-    <saveButton/>
   </main>
 </template>
