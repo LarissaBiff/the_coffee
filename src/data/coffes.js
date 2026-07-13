9d1a5bb06470c4ba5bda89c735a001334ff5b628
LarissaBiff <bifflarissa@gmail.com>
2026-07-13 16:50:28 -0300

    I made the coffees list (coffees.js), the coffeType.vue - selection;

---
+++ src/data/coffes.js
@@ -0,0 +1,30 @@
+export const cafes = [
+    {
+        id: 1,
+        nome: 'Bourbon Amarelo',
+        origem: "Minas Gerais - BR",
+        avaliado: false,
+        media: 8.7
+    },
+    {
+        id: 2,
+        nome: 'Arara',
+        origem: "Espirito Santo - BR",
+        avaliado: false,
+        media: 8.3
+    },
+    {
+        id: 3,
+        nome: 'Catuaí',
+        origem: "Paraná - BR",
+        avaliado: false,
+        media: 7.9
+    },
+    {
+        id: 4,
+        nome: 'Mundo Novo',
+        origem: "Bahia - BR",
+        avaliado: false,
+        media: 7.2
+    },
+]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import aGrade from '@/inputs/aGrade.vue';
+import coffeeType from '@/inputs/coffeeType.vue';
 </script>
 
 <template>
   <main>
     <aGrade/>
+    <coffee-type/>
   </main>
 </template>
