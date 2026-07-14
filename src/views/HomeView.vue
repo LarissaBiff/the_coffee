96864aaf6e058fe88654a6c3b38f63563d191f35
LarissaBiff <bifflarissa@gmail.com>
2026-07-14 20:41:18 -0300

    Just to view...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <script setup>
+import rateButton from '@/botoes/rateButton.vue';
+import saveButton from '@/botoes/saveButton.vue';
+import seeCoffees from '@/botoes/seeCoffees.vue';
 </script>
 
 <template>
   <main>
+    <rate-button/>
+    <save-button/>
+    <see-coffees/>
   </main>
 </template>
+<style>
+main{
+  display: flex;
+  flex-direction: column;
+  gap: 2vw;
+}
+</style>
