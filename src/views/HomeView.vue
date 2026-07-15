a3bf1eb74ba3e32ad21f6c9edade74d67ce88c0f
LarissaBiff <bifflarissa@gmail.com>
2026-07-14 21:04:35 -0300

    ...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,27 @@
 <script setup>
+import aGrade from '@/inputs/aGrade.vue';
+import coffeeType from '@/inputs/coffeeType.vue';
+import competingCoffees from '@/Shows/competingCoffees.vue';
+import currentAverage from '@/Shows/currentAverage.vue';
+import rankingStructure from '@/Shows/rankingStructure.vue';
+import recordedRates from '@/Shows/recordedRates.vue';
 </script>
 
 <template>
   <main>
+    <aGrade/>
+    <coffee-type/>
+    <competing-coffees/>
+    <current-average/>
+    <ranking-structure/>
+    <recorded-rates/>
   </main>
 </template>
 
 <style scoped>
-
+main{
+  display: flex;
+  flex-direction: column;
+  gap: 2vw;
+}
 </style>
